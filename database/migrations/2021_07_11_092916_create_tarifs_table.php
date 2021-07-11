6d81a5f15fc0a79e64bc3805677008f0a71b6202
Alvif Sandana Mahardika <asm240800@gmail.com>
2021-07-11 19:11:13 +0700

    make migration tarif table and keterangan column

---
+++ database/migrations/2021_07_11_092916_create_tarifs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTarifsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarif', function (Blueprint $table) {
+            $table->id('idTarif');
+            $table->string('titik_pengiriman');
+            $table->string('tujuan_pengiriman');
+            $table->integer('tarif');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarif');
+    }
+}
