14601032bbb6e8c3302f8fb03c576910b375765e
AlvifSandana <asm240800@gmail.com>
2021-10-31 12:27:27 +0700

    get data jadwal and return to view

--- app/Http/Controllers/JadwalController.php
+++ app/Http/Controllers/JadwalController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class JadwalController extends Controller
 {
@@ -10,7 +11,13 @@ class JadwalController extends Controller
      * show penjadwalan page
      */
     public function index(){
-        return view('admin.penjadwalan');
+        $data_jadwal = DB::table('jadwal')
+                          ->join('kendaraan', 'jadwal.Kendaraan_idKendaraan', '=', 'kendaraan.idKendaraan')
+                          ->join('supir', 'jadwal.Kendaraan_Supir_idSupir', '=', 'supir.idSupir')
+                          ->select('jadwal.idJadwal', 'kendaraan.*', 'supir.*')
+                          ->get();
+        // return json_encode($data_jadwal);
+        return view('admin.penjadwalan', compact('data_jadwal'));
     }
 
     public function show(){
