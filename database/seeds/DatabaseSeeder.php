663c62f1b9dfdee53bfe3f7b46ae1acb717fde80
AlvifSandana <asm240800@gmail.com>
2021-11-18 05:04:41 +0700

    update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(AdminSeeder::class);
+        $this->call(SupirSeeder::class);
+        $this->call(KendaraanSeeder::class);
+        $this->call(PengirimSeeder::class);
     }
 }
