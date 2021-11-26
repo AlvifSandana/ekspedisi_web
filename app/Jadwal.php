6cc759c6b4cd4192c2b802e4e6acb386343a074c
AlvifSandana <asm240800@gmail.com>
2021-11-27 00:36:34 +0700

    update Jadwal model: add tanggal_pemberangkatan column

--- app/Jadwal.php
+++ app/Jadwal.php
@@ -12,6 +12,8 @@ class Jadwal extends Model
     protected $primaryKey = 'idJadwal';
     // mass-assignment
     protected $fillable = [
-        'Admin_idAdmin', 'Kendaraan_idKendaraan', 'Kendaraan_Supir_idSupir'
+        'Admin_idAdmin', 'Kendaraan_idKendaraan', 'Kendaraan_Supir_idSupir', 'tanggal_pemberangkatan'
     ];
+    // date columns
+    protected $dates = ['tanggal_pemberangkatan'];
 }
