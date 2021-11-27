8fbe2f2e051ab23cf86ca3415dde0a97b1f67f3b
AlvifSandana <asm240800@gmail.com>
2021-11-27 21:45:05 +0700

    topbar: remove notification

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -47,7 +47,7 @@
         </li>
 
         <!-- Nav Item - Alerts -->
-        <li class="nav-item dropdown no-arrow mx-1">
+        {{-- <li class="nav-item dropdown no-arrow mx-1">
             <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fas fa-bell fa-fw"></i>
@@ -99,7 +99,7 @@
 
 
 
-        <div class="topbar-divider d-none d-sm-block"></div>
+        <div class="topbar-divider d-none d-sm-block"></div> --}}
 
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
