e83f3d798e7607457309c9044e1816106d0b419b
AlvifSandana <asm240800@gmail.com>
2022-01-03 14:29:03 +0700

    new method (Transaksi model)

--- app/Transaksi.php
+++ app/Transaksi.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Transaksi extends Model
 {
@@ -11,9 +12,30 @@ class Transaksi extends Model
     // otomatis mengisi kolom timestamps
     public $timestamps = true;
     // primary key
-    protected $primaryKey = 'id_Transaksi';
+    protected $primaryKey = 'idTransaksi';
     // mass-assignment
     protected $fillable = [
         'Admin_idAdmin', 'Barang_idBarang', 'Barang_Pengirim_idPengirim', 'status'
     ];
+
+    public function byId(String $idTransaksi)
+    {
+        try {
+            $result = 'Data tidak ditemukan!';
+            // get data transaksi
+            $data_transaksi = DB::table('transaksi')
+                ->join('barang', 'Barang_idBarang', '=', 'barang.idBarang')
+                ->join('pengirim', 'Barang_Pengirim_idPengirim', '=', 'pengirim.idPengirim')
+                ->select('transaksi.*', 'barang.*', 'pengirim.*')
+                ->where('idTransaksi', '=', $idTransaksi)
+                ->get();
+            if (sizeof($data_transaksi) > 0) {
+                $result = $data_transaksi;
+                return $result;
+            }
+            return $result;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
 }
