bc52cd34ed3c03051e73a0a3889c9bee4eda3f43
AlvifSandana <asm240800@gmail.com>
2022-01-03 14:28:43 +0700

    add register method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,8 @@ class AuthController extends Controller
     /**
      * show login form
      */
-    public function showLoginForm(){
+    public function showLoginForm()
+    {
         if (Auth::check()) {
             return redirect()->route('admin.dashboard.index');
         }
@@ -23,7 +24,8 @@ class AuthController extends Controller
     /**
      * handle login
      */
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         // validation rules
         $rules = [
             'email'     => 'required|email',
@@ -50,7 +52,8 @@ class AuthController extends Controller
             if (Hash::check($login_data['password'], $admin->password)) {
                 Auth::attempt($login_data);
                 if (Auth::check()) {
-                    $request->session()->put('name', $admin->name);
+                    $request->session()->put('name', $admin->nama_admin);
+                    $request->session()->put('id', $admin->idAdmin);
                     return redirect()->route('admin.dashboard.index')->with('success', 'Selamat datang, Admin.');
                 } else {
                     return redirect()->route('login.form')->with('error', 'Email atau password salah!');
@@ -62,9 +65,52 @@ class AuthController extends Controller
     }
 
     /**
+     * handle register
+     */
+    public function register(Request $request)
+    {
+        // validation rules
+        $rules = [
+            'nama_admin'      => 'required',
+            'email'     => 'required|email',
+            'password'  => 'required'
+        ];
+        // custom error message for validation
+        $messages = [
+            'required'      => ':attribute tidak boleh kosong!',
+            'email.email'   => 'Email tidak valid!'
+        ];
+        // create validator instance
+        $validator = Validator::make($request->all(), $rules, $messages);
+        // validation process
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput($request->all());
+        }
+        // login data
+        $login_data = [
+            'name'      => $request->input('name'),
+            'email'     => $request->input('email'),
+            'password'  => $request->input('password')
+        ];
+        // create model instance
+        $admin = new Admin;
+        // insert data
+        $admin->name = $login_data['name'];
+        $admin->email = $login_data['email'];
+        $admin->password = Hash::make($login_data['password']);
+        // save data
+        if ($admin->save()) {
+            return redirect()->route('login.form')->with('success', 'Selamat datang, Admin.');
+        } else {
+            return redirect()->route('login.form')->with('error', 'Email atau password salah!');
+        }
+    }
+
+    /**
      * handle logout
      */
-    public function logout(){
+    public function logout()
+    {
         Auth::logout();
         return redirect()->route('login.form');
     }
